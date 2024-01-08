21d4f1fecb19abd6750d14b75f21679b23262ad1
FernandoAlmeida21 <fernandoalmeida@fisica.ufc.br>
2024-01-08 16:03:33 -0300

    fix: footer and some other styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,23 @@
-import styles from "./footer.module.css"
+import styles from "./footer.module.css";
 
 export default function Footer() {
-    return (
-        <footer className={styles.container}>
-            <div className={styles.footerText}>
-                <div>R. Vinte e Cinco de Março, 268 - Centro,<br />Fortaleza - CE, 60060-120</div>
-                <div>observatoriodefortaleza@iplanfor.fortaleza.ce.gov.br</div>
-            </div>
-            <div>
-                <img src="support.svg" alt="Colaboradores" />
-            </div>
-        </footer>
-    )
+  return (
+    <footer className={styles.container}>
+      <div className={styles.footerText}>
+        <div>
+          R. Vinte e Cinco de Março, 268 - Centro,
+          <br />
+          Fortaleza - CE, 60060-120
+        </div>
+        <div>observatoriodefortaleza@iplanfor.fortaleza.ce.gov.br</div>
+      </div>
+      <div className={styles.logos}>
+        <div className={styles.creators}>
+          <img src="iplanfor.png" alt="Iplanfor" />
+          <img src="prefeitura.png" alt="Prefeitura" />
+        </div>
+        <img src="colaboradores.png" alt="Colaboradores" />
+      </div>
+    </footer>
+  );
 }
-
